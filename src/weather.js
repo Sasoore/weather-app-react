19508b440457779789fe750e3c7f6b986ac2e226
Sara <118803542+Sasoore@users.noreply.github.com>
2022-12-18 14:52:41 +0330

    new project

---
+++ src/weather.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import axios from 'axios'
+import Loader from 'react-loader-spinner'
+
+export default function Weather() {
+  function handleSubmit(response) {
+    alert(`Weather in london is ${response.data.main.temp}`)
+  }
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=c5eae455c0d84c0de87118e8f84251f7`
+  axios.get(apiUrl).then(handleSubmit)
+}
